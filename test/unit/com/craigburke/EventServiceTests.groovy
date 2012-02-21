8ca49464fbe368f53d630a17257b15b51b5b0568
Craig Burke <craig@craigburke.com>
2012-02-21 15:30:58 -0500

    Adding EventServiceTest file

---
+++ test/unit/com/craigburke/EventServiceTests.groovy
@@ -0,0 +1,17 @@
+package com.craigburke
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
+ */
+@TestFor(EventService)
+class EventServiceTests {
+
+    void testSomething() {
+        fail "Implement me"
+    }
+}
