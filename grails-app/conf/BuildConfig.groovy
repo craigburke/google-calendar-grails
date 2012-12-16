c34c7fe02112968bef25ee32cb3680f96855e176
Craig Burke <craig@craigburke.com>
2012-12-16 12:14:56 -0500

    Upgrading project to grails 2.1.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -29,6 +29,7 @@ grails.project.dependency.resolution = {
         mavenRepo "http://repository.codehaus.org"
         mavenRepo "http://download.java.net/maven/2/"
         mavenRepo "http://repository.jboss.com/maven2/"
+        mavenRepo "https://repo.springsource.org/repo"
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -33,46 +33,6 @@ grails.mime.types = [ html: ['text/html','application/xhtml+xml'],
 
 // What URL patterns should be processed by the resources plugin
 grails.resources.adhoc.patterns = ['/images/*', '/css/*', '/js/*', '/plugins/*']
-grails.resources.modules = {
-    core {
-        resource url:'/js/jquery-1.7.1.min.js', disposition: 'head'
-        resource url:'/js/jquery-ui-1.8.18.custom.min.js', disposition: 'head'
-
-        resource url: '/css/smoothness/jquery-ui-1.8.18.custom.css'
-    }
-
-    home {
-        resource url:'/css/home.css'
-    }
-
-    datePicker {
-        dependsOn 'core'
-        resource url: '/js/jquery-ui-timepicker-addon.js'
-    }
-
-
-    fullCalendar {
-        dependsOn 'core'
-        resource url:'/js/fullcalendar.min.js'
-        resource url:'/css/fullcalendar.css'
-    }
-
-    qtip {
-        dependsOn 'core'
-
-        resource url: '/js/jquery.qtip.min.js'
-        resource url: '/css/jquery.qtip.min.css'
-    }
-
-
-    calendar {
-        dependsOn 'fullCalendar, datePicker, qtip'
-
-        resource url: '/js/calendar.js'
-        resource url: '/css/calendar.css'
-    }
-
-}
 
 // The default codec used to encode data with ${}
 grails.views.default.codec = "none" // none, html, base64
